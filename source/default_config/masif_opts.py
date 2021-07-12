f74334a5bc71df524b6856f8ef7fe23820acbbbf
Adithyan Unni <63242033+justadithyan@users.noreply.github.com>
2021-07-12 14:54:02 +0530

    au changes
    Added location for vertex-residue index mapping dictionaries

--- source/default_config/masif_opts.py
+++ source/default_config/masif_opts.py
@@ -5,6 +5,7 @@ masif_opts = {}
 masif_opts["raw_pdb_dir"] = "data_preparation/00-raw_pdbs/"
 masif_opts["pdb_chain_dir"] = "data_preparation/01-benchmark_pdbs/"
 masif_opts["ply_chain_dir"] = "data_preparation/01-benchmark_surfaces/"
+masif_opts["res_index_dir"] = "data_preparation/01-benchmark_index_dicts/"
 masif_opts["tmp_dir"] = tempfile.gettempdir()
 masif_opts["ply_file_template"] = masif_opts["ply_chain_dir"] + "/{}_{}.ply"
 
